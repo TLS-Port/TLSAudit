5f04ec7ce5aa4db790b9bbaf8acb29faa15fc460
Paul Duplys <paul.duplys@bosch.com>
2024-09-15 21:58:46 +0200

    Update the TLSAudit script
    Source parser, checker and reporter code and call the corresponding functions (which have been tested with corresponding unit tests).

--- tlsaudit.sh
+++ tlsaudit.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Check if an argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <nginx_config_file>"
+    exit 1
+fi
+
+# Read the Nginx configuration file from the command line argument
+nginx_config_file="$1"
+
 source ./lib/parser.sh
 source ./lib/checker.sh
 source ./lib/reporter.sh
 
-# Call the function
-parseNginxConfig ./test/nginx_example1.conf
-
-# Print the array
-#echo "Array elements: ${tls_options[@]}"
-# Loop through the array and print each element on a new line
-for element in "${tls_options[@]}"; do
-    echo "$element"
-done
+# Runs TLS audit with the provided Nginx configuration file
+parseNginxConfig "$nginx_config_file"
+checkNginxTLSOptions "$tls_options"
+reportWeakTLSOptions
